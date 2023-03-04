d7c0cf804f577471769cd031fb1d180842be347b
Khairmadan <91107758+Khairmadan@users.noreply.github.com>
2023-03-04 15:23:17 +0700

    Update komputasi fungsi gelombang kuantum.py

--- komputasi fungsi gelombang kuantum.py
+++ komputasi fungsi gelombang kuantum.py
@@ -1,21 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#computation of the  wave function quantum
+#computation of the function quantum
 a = 2 *(0.529 * 10**(-10)) #m
 m = 9.109 * 10**(-31) #kg mass of the electron
-h = 6.626 * 10**(-34) #J.s Planck constant
+h = 6.626 * 10**(-34)/ (2*np.pi) #J.s Planck constant
 x = np.linspace(0, a, 1000)
 E = np.zeros(3)
 psi = np.zeros((3, len(x)))
+figure, axis = plt.subplots(3,1)
 for n in range(1,4):
-    E[n-1] = (n**2 * np.pi**2 * h**2)/(2 * m * a**2)/1.6*10**(-19)
+    E[n-1] = ((n**2) * ((np.pi)**2) * (h**2))/((2 * m * (a**2))) * (6.242*10**18)
     for i in range(len(x)):
         psi[n-1, i] = (np.sqrt(np.pi/a) * np.sin(n * np.pi * x[i]/a))**2
-plt.plot(x/a,psi[0], label = 'n = 1')
-plt.plot(x/a,psi[1], label = 'n = 2')
-plt.plot(x/a,psi[2], label = 'n = 3')
-plt.xlabel('x (m)')
-plt.ylabel('ψ(x)')
-plt.legend()
+    axis[n-1].plot(x / a, psi[n-1], color = 'red', linewidth = 2)
+    axis[n-1].set_title(' E = %0.1i' % E[n-1] + ' eV')
+    axis[n-1].grid(True)
+    axis[n-1].set_xlabel('x (m)')
+    axis[n-1].set_ylabel('psi')
+
+
+
 plt.show()
